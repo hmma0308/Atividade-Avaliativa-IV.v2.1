dbc4f4babf607946752cb8e1f05fb649d76b1fa5
Hugo Mamona <113359386+hmma0308@users.noreply.github.com>
2025-05-13 16:22:55 +0000

    add example request for task

--- example_requests/server_example_requests/tasks/New_Task.sh
+++ example_requests/server_example_requests/tasks/New_Task.sh
@@ -1,5 +1,6 @@
 curl --request POST \
   --url https://atividade-avaliativa-iv-v2-1.vercel.app/tasks \
+  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NDcxNTI3NDgsImV4cCI6MTc0NzE1NjM0OH0.lKpZBmQx5uGHnEMTOE7j8hrEGFhcdOkGrG_MNuYStR0' \
   --header 'Content-Type: application/json' \
   --data '{
     "title": "Complete project",

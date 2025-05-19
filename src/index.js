fe24c38662a654a6ddad581f3cce5393a3e56c06
Hugo Mamona <113359386+hmma0308@users.noreply.github.com>
2025-05-19 00:10:53 +0000

    integration with frontend

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import db from './models/index.js'
+import userRoute from './routes/user.routes.js'
+import exampleRoute from './routes/example.routes.js';
+import taskRoute from './routes/task.routes.js';
+import swaggerRoute from './routes/swagger.route.js';
+
+db.sequelize.sync()
+    .then(() => {
+        console.log("Database synced successfully.");
+    })
+.catch((error) => {
+    console.error("Error syncing database:", error);
+});
+
+const app = express();
+app.use(express.json());
+app.use('/users', userRoute);
+app.use("/secureExampleRoute", exampleRoute);
+app.use('/tasks', taskRoute);
+app.use(swaggerRoute);
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.get('/', (req, res) => {
+    res.send({message: 'Hello World!'});
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(3000, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
